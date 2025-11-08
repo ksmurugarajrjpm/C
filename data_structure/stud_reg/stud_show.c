2bf9292fe974bc9f8a6dcfe0e651eae99c230db8
Murugaraj <125633698+ksmurugarajrjpm@users.noreply.github.com>
2025-11-08 16:51:42 +0530

    Add files via upload

---
+++ data_structure/stud_reg/stud_show.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+void stud_show()
+{
+    if(header == NULL)
+        {
+            printf("\n");
+            printf("List was empty...!");
+            printf("\n");
+            sel_choice();
+        }
+
+    st* temp = header;
+
+    printf("\n");
+    printf("Roll.no\t\t\t");
+    printf("Name\t\t\t\t\t");
+    printf("Percentage");
+    printf("\n");
+
+    while(temp != NULL)
+    {
+        printf("%d\t\t\t", temp->roll);
+        printf("%s\t\t\t\t\t", temp->name);
+        printf("%.2f", temp->per);
+        temp = temp->next;
+        printf("\n");
+    }
+
+    sel_choice();
+
+}
